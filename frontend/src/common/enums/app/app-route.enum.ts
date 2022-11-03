35615359e545e9d218f3034732f0d08cf07478a2
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2022-11-03 13:57:45 +0200

    frontend/redux-setup: added routes, route enum and scroll to top

---
+++ frontend/src/common/enums/app/app-route.enum.ts
@@ -0,0 +1,8 @@
+enum AppRoute {
+  ROOT = '/',
+  SIGN_IN = '/sign-in',
+  SIGN_UP = '/sign-up',
+  ANY = '*',
+}
+
+export { AppRoute };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/app/app';
+import { Provider } from 'react-redux';
+import { persistor, store } from './store/store';
 
-import 'assets/styles/styles.scss'; 
+import 'assets/styles/styles.scss';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
