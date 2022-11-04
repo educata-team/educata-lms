da78cdce2cc5e0ae686ebd977949de9dc62488a0
Ihor Pavliuk <nemuzora.nozomu@gmail.com>
2022-11-04 12:31:13 +0200

    frontend: + icon usage example

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,5 +1,6 @@
-import { AppRoute } from 'common/enums/enums';
+import { AppRoute, IconColor, IconName } from 'common/enums/enums';
 import { FC } from 'common/types/types';
+import { Icon } from 'components/common/icon/icon';
 import { Route, Routes } from 'react-router-dom';
 import { ScrollToTop } from './scroll-to-top';
 
@@ -11,6 +12,10 @@ export const App: FC = () => {
   return (
     <>
       <ScrollToTop querySelectors={scrollToTopQuerySelectors} />
+      {/* Remove this later */}
+      <Icon name={IconName.HOME} size={20} color="green" />
+      <Icon name={IconName.BELL} size={50} color={IconColor.PRIMARY_BLUE} />
+      
       <div className={styles['main-content']}>
         <Routes>
           <Route path={AppRoute.ROOT} element={<></> /* Insert homepage component here instead of <></> */} />
