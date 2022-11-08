c6bf8e12835cc714fde388ca4a40eb1aa6577754
Illya Shuba <105970858+IlliaShuba@users.noreply.github.com>
2022-11-08 20:52:27 +0200

    registration pages

--- frontend/src/components/pages/auth/registration.tsx
+++ frontend/src/components/pages/auth/registration.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'common/types/types';
 import React, { useCallback, useState } from 'react';
-import FirstPage from './registration-pages/first-page';
-import SecondPage from './registration-pages/second-page';
+import { FirstPage } from './registration-pages/first-page';
+import { SecondPage } from './registration-pages/second-page';
 
 export const Registration:FC = () => {
   const [username, setUsername] = useState<string>();
@@ -14,12 +14,13 @@ export const Registration:FC = () => {
   const goToPage = useCallback((pageNumber: number) => {setPage(pageNumber);}, [setPage]);
   const getInfoFromFirstPage = useCallback((email: string, password: string) => {setEmail(email); setPassword(password);}, [setEmail, setPassword]);
   const getInfoFromSecondPage = useCallback((username: string, first_name: string, last_name: string) => {setUsername(username); setFirst_name(first_name); setLast_name(last_name);}, [setUsername, setFirst_name, setLast_name]);
+  const submitClick = useCallback(async () => {fetch('http://localhost:8000/api/v1/users/login/',{ method: 'POST', body: JSON.stringify({ username: username, first_name: first_name, last_name: last_name, email:email, password: password }) });}, [email, first_name, last_name, password, username]);
 
   switch (page) {
     case 1:
       return <FirstPage goToPage={goToPage} setInfoFromFirstPage={getInfoFromFirstPage}/>;
     case 2:
-      return <SecondPage goToPage={goToPage} setInfoFromSecondPage={getInfoFromSecondPage}/>;
+      return <SecondPage goToPage={goToPage} setInfoFromSecondPage={getInfoFromSecondPage} submitClick={submitClick}/>;
     default:
       return <FirstPage goToPage={goToPage} setInfoFromFirstPage={getInfoFromFirstPage}/>;
   }
