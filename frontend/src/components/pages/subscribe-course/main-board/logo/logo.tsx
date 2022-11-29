0b46daec9315a2709b5b0c72f13dfc008f8c1766
Illya Shuba <105970858+IlliaShuba@users.noreply.github.com>
2022-11-29 23:06:59 +0200

    subscribe-course page / logo

--- frontend/src/components/pages/subscribe-course/main-board/logo/logo.tsx
+++ frontend/src/components/pages/subscribe-course/main-board/logo/logo.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import styles from '../styles.module.scss';
 import { FC } from '../../../../../common/types/react/fc.type';
 
-export const Logo:FC = () => {
+interface Props{
+  url: string;
+}
+
+export const Logo:FC<Props> = ({
+  url,
+                               }) => {
   return (
-    <div>
-      Пропонує
-      <div>tut logo</div>
+    <div className={styles['logo']}>
+      <p>Пропонує</p>
+      <img src={url} alt={'course logo'}/>
     </div>
   );
 };
--- frontend/src/components/pages/subscribe-course/main-board/main-board.tsx
+++ frontend/src/components/pages/subscribe-course/main-board/main-board.tsx
@@ -9,7 +9,7 @@ export const MainBoard:FC = () => {
     <div className={styles['main-board-wrapper']}>
       <div className={styles['main-board']}>
         <Review/>
-        <Logo/>
+        <Logo url={'https://images-platform.99static.com//KlBLMX8dQrcq6hZGnxf5HSnG29I=/8x543:525x1060/fit-in/500x500/99designs-contests-attachments/123/123360/attachment_123360235'}/>
       </div>
     </div>
   );
