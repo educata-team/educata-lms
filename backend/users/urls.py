38c17dd2e075e25fba0abf55d3d38e757e0c1998
K0lles <oleksijkolotilo63@gmail.com>
2022-11-29 15:36:37 +0200

    added get request by id for user

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -7,5 +7,6 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path('registration/', UserRegistrationAPIView.as_view(), name='user_registration'),
     path('login/', MyTokenObtainPairView.as_view(), name='login'),
-    path('refresh/', MyRefreshToken.as_view(), name='refresh-token')
+    path('refresh/', MyRefreshToken.as_view(), name='refresh-token'),
+    path('user/<int:user_pk>/', UserRetrieveAPIView.as_view(), name='user-detail'),
 ]
